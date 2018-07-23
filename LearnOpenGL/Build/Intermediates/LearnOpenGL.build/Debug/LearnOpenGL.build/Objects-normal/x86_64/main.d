f523adc41b23ace0efc2bed8e6ee63823712357b
Todor Ivanov <todor.ivanov066@gmail.com>
2018-07-23 21:11:50 +0300

    Remove the now ignored directory Build

--- LearnOpenGL/Build/Intermediates/LearnOpenGL.build/Debug/LearnOpenGL.build/Objects-normal/x86_64/main.d
+++
@@ -1,4 +0,0 @@
-dependencies: \
-  /Users/todorivanov/Documents/Graphics/LearnOpenGL/LearnOpenGL/main.cpp \
-  /Users/todorivanov/Documents/Graphics/daVinci_Header/daVinci.h \
-  /Users/todorivanov/Documents/Graphics/LearnOpenGL/Dependecies/GLFW/headers/glfw3.h
